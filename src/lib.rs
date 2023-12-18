4093b45720f6dd0f58f0740ec9920bff998a5cd8
AuroraEchoes <auroraesmerelda@gmail.com>
2023-12-18 13:21:21 +1100

    Impl Add, AddAssign, Div, Divassign for IVec2 with tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, AddAssign};
+use std::ops::{Add, AddAssign, Div, DivAssign};
 
 pub trait Vector {
     type Coord;
@@ -61,6 +61,7 @@ impl Vector for IVec2 {
     }
 }
 
+// Add
 impl Add<IVec2> for IVec2 {
     type Output = IVec2;
 
@@ -76,11 +77,59 @@ impl AddAssign<IVec2> for IVec2 {
     }
 }
 
-#[macro_export]
-macro_rules! ivec {
-    ($a: expr, $b: expr) => {
-        IVec2::new($a, $b)
-    };
+impl Add<i32> for IVec2 {
+    type Output = IVec2;
+
+    fn add(self, rhs: i32) -> Self::Output {
+        return Self::new(self.x() + rhs, self.y() + rhs);
+    }
+}
+
+impl AddAssign<i32> for IVec2 {
+    fn add_assign(&mut self, rhs: i32) {
+        self.x += rhs;
+        self.y += rhs;
+    }
+}
+
+// Divide
+impl Div<IVec2> for IVec2 {
+    type Output = IVec2;
+
+    fn div(self, rhs: IVec2) -> Self::Output {
+        return Self::new(self.x() / rhs.x(), self.y() / rhs.y());
+    }
+}
+
+impl DivAssign<IVec2> for IVec2 {
+    fn div_assign(&mut self, rhs: IVec2) {
+        self.x /= rhs.x();
+        self.y /= rhs.y();
+    }
+}
+
+impl Div<i32> for IVec2 {
+    type Output = IVec2;
+
+    fn div(self, rhs: i32) -> Self::Output {
+        return Self::new(self.x() / rhs, self.y() / rhs);
+    }
+}
+
+impl DivAssign<i32> for IVec2 {
+    fn div_assign(&mut self, rhs: i32) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
+}
+
+pub mod macros {
+    #[macro_export]
+    macro_rules! ivec {
+        ($a: expr, $b: expr) => {
+            IVec2::new($a, $b)
+        };
+    }
 }
 
 #[cfg(test)]
@@ -92,5 +141,29 @@ mod tests {
         let vec = ivec!(2, 2);
         let equiv = IVec2::new(2, 2);
         assert_eq!(vec, equiv);
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(ivec!(2, 5), ivec!(1, 4) + ivec!(1, 1));
+        let mut add_eq = ivec!(1, 1);
+        add_eq += ivec!(2, 4);
+        assert_eq!(ivec!(3, 5), add_eq);
+        assert_eq!(ivec!(2, 2), ivec!(1, 1) + 1);
+        let mut add_eq_i32 = ivec!(1, 1);
+        add_eq_i32 += 1;
+        assert_eq!(ivec!(2, 2), add_eq_i32);
+    }
+
+    #[test]
+    fn test_divide() {
+        assert_eq!(ivec!(3, 2), ivec!(9, 8) / ivec!(3, 4));
+        let mut div_eq = ivec!(9, 8);
+        div_eq /= ivec!(3, 4);
+        assert_eq!(ivec!(3, 2), div_eq);
+        assert_eq!(ivec!(2, 4), ivec!(4, 8) / 2);
+        let mut div_eq_i32 = ivec!(2, 4);
+        div_eq_i32 /= 2;
+        assert_eq!(ivec!(1, 2), div_eq_i32);
     }
 }
